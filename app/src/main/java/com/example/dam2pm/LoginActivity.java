7a6441f403603b03c0b5f281009fa59dc58885d9
FlorenciaFigueroaI <72564481+FlorenciaFigueroaI@users.noreply.github.com>
2020-10-18 15:25:08 +0200

    Modificación LoginActivity.java
    * Se agrega setVisibility para el checkBox (mantener_sesion_iniciada)

--- app/src/main/java/com/example/dam2pm/LoginActivity.java
+++ app/src/main/java/com/example/dam2pm/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.dam2pm;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -15,29 +16,32 @@ public class LoginActivity extends AppCompatActivity {
     EditText txt;
     EditText txt2;
     TextView txtVw;
+    CheckBox chkBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        // Cargar el fragmento de registro al clikear en el botón de registro
+
         btn=(Button) findViewById(R.id.btnRegistro);
         btn2=(Button) findViewById(R.id.btnEntrar);
         txt=(EditText) findViewById(R.id.txtEmailUsuario);
         txt2=(EditText) findViewById(R.id.txtPwd);
         txtVw=(TextView) findViewById(R.id.txtVwOlvidoPwd);
+        chkBox=(CheckBox) findViewById(R.id.chkBoxMantenerSesion);
+
         btn.setOnClickListener(new View.OnClickListener() {
 
             @Override
-            public void onClick(View v) {
-                btn.setVisibility(View.GONE);
+            public void onClick(View v) {        // Carga el fragmento de registro al clikear en el botón de registro
+                btn.setVisibility(View.GONE);    // Desaparecen los componentes de la vista del usuario
                 btn2.setVisibility(View.GONE);
                 txt.setVisibility(View.GONE);
                 txt2.setVisibility(View.GONE);
                 txtVw.setVisibility(View.GONE);
-         //       FragmentManager frg = getSupportFragmentManager(); // instancia del administrador de fragmentos
+                chkBox.setVisibility(View.GONE);
                 FragmentTransaction transaccion = getSupportFragmentManager().beginTransaction(); // creación nueva transacción
-                RegistroFragment frgRegistro = new RegistroFragment();
+                RegistroFragment frgRegistro = new RegistroFragment(); // instancia de transacción
                 transaccion.add(R.id.contenedor, frgRegistro); // se añade transacción
                 transaccion.commit(); // confirmación del cambio
 

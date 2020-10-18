dd53ef4c8c7121c785a41b4e390d01ef133778ab
FlorenciaFigueroaI <72564481+FlorenciaFigueroaI@users.noreply.github.com>
2020-10-18 14:40:07 +0200

    Modificación Main Activity
    * Se cambia el nombre de la MainActivity y el Layout de activity_main por LoginActivity y activity_login
    * Se agrega un Activity que se nombra como MainActivity

---
+++ app/src/main/java/com/example/dam2pm/LoginActivity.java
@@ -0,0 +1,51 @@
+package com.example.dam2pm;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+public class LoginActivity extends AppCompatActivity {
+    Button btn;
+    Button btn2;
+    EditText txt;
+    EditText txt2;
+    TextView txtVw;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        // Cargar el fragmento de registro al clikear en el botón de registro
+        btn=(Button) findViewById(R.id.btnRegistro);
+        btn2=(Button) findViewById(R.id.btnEntrar);
+        txt=(EditText) findViewById(R.id.txtEmailUsuario);
+        txt2=(EditText) findViewById(R.id.txtPwd);
+        txtVw=(TextView) findViewById(R.id.txtVwOlvidoPwd);
+        btn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                btn.setVisibility(View.GONE);
+                btn2.setVisibility(View.GONE);
+                txt.setVisibility(View.GONE);
+                txt2.setVisibility(View.GONE);
+                txtVw.setVisibility(View.GONE);
+         //       FragmentManager frg = getSupportFragmentManager(); // instancia del administrador de fragmentos
+                FragmentTransaction transaccion = getSupportFragmentManager().beginTransaction(); // creación nueva transacción
+                RegistroFragment frgRegistro = new RegistroFragment();
+                transaccion.add(R.id.contenedor, frgRegistro); // se añade transacción
+                transaccion.commit(); // confirmación del cambio
+
+            }
+        });
+
+    }
+
+
+
+}
--- app/src/main/java/com/example/dam2pm/MainActivity.java
+++ app/src/main/java/com/example/dam2pm/MainActivity.java
@@ -1,52 +1,14 @@
 package com.example.dam2pm;
 
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-
-import androidx.annotation.ContentView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    Button btn;
-    Button btn2;
-    EditText txt;
-    EditText txt2;
-    TextView txtVw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Cargar el fragmento de registro al clikear en el botón de registro
-        btn=(Button) findViewById(R.id.btnRegistro);
-        btn2=(Button) findViewById(R.id.btnEntrar);
-        txt=(EditText) findViewById(R.id.txtEmailUsuario);
-        txt2=(EditText) findViewById(R.id.txtPwd);
-        txtVw=(TextView) findViewById(R.id.txtVwOlvidoPwd);
-        btn.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                btn.setVisibility(View.GONE);
-                btn2.setVisibility(View.GONE);
-                txt.setVisibility(View.GONE);
-                txt2.setVisibility(View.GONE);
-                txtVw.setVisibility(View.GONE);
-         //       FragmentManager frg = getSupportFragmentManager(); // instancia del administrador de fragmentos
-                FragmentTransaction transaccion = getSupportFragmentManager().beginTransaction(); // creación nueva transacción
-                RegistroFragment frgRegistro = new RegistroFragment();
-                transaccion.add(R.id.contenedor, frgRegistro); // se añade transacción
-                transaccion.commit(); // confirmación del cambio
-
-            }
-        });
-
     }
-
-
-
 }

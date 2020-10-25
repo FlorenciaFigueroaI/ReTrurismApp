252165f884990dd35b79b78ae6b9f01321be4408
FlorenciaFigueroaI <72564481+FlorenciaFigueroaI@users.noreply.github.com>
2020-10-25 10:09:38 +0100

    Cambios en Main
    * Se agrega ConfiguracionActivity

--- app/src/main/java/com/example/dam2pm/ConfiguracionActivity.java
+++ app/src/main/java/com/example/dam2pm/ConfiguracionActivity.java
@@ -3,6 +3,9 @@ package com.example.dam2pm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TableLayout;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class ConfiguracionActivity extends AppCompatActivity {
 
@@ -10,5 +13,8 @@ public class ConfiguracionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configuracion);
+
+
+
     }
 }

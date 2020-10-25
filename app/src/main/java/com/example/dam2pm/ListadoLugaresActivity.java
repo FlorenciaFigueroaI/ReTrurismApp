491015dff503bc76e0208f86e17e1887a9358d13
FlorenciaFigueroaI <72564481+FlorenciaFigueroaI@users.noreply.github.com>
2020-10-25 12:02:11 +0100

    Cambios en activity_configuracion.xml
    * Se agregan tres CardView y dos Horizontal Divider
    * Se agregan Spinner, RadioGroup, RadioBotton y SwitchBotton

--- app/src/main/java/com/example/dam2pm/ListadoLugaresActivity.java
+++ app/src/main/java/com/example/dam2pm/ListadoLugaresActivity.java
@@ -1,14 +1,25 @@
 package com.example.dam2pm;
 
+import android.os.Bundle;
+import android.widget.Toolbar;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.util.Objects;
 
 public class ListadoLugaresActivity extends AppCompatActivity {
-
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listado_lugares);
+
+        // Toolbar
+        toolbar = findViewById(R.id.toolbar1);
+        setSupportActionBar();
+        Objects.requireNonNull(getActionBar()).setTitle("Ayer & Hoy");
+    }
+
+    private void setSupportActionBar() {
     }
 }

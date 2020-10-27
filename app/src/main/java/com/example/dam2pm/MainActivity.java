3fb65677ffbb1dd8fba640e425eeeedc685bd278
FlorenciaFigueroaI <72564481+FlorenciaFigueroaI@users.noreply.github.com>
2020-10-27 21:38:05 +0100

    Cambios en strings.xml
    * Se agregan los idiomas inglés y francés. strings_en.xml, strings_fr.xml

--- app/src/main/java/com/example/dam2pm/MainActivity.java
+++ app/src/main/java/com/example/dam2pm/MainActivity.java
@@ -1,26 +1,21 @@
 package com.example.dam2pm;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.Toolbar;
+//import android.widget.Toolbar;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Objects;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private Toolbar toolbar;
-    private int ACTIVITY_PRINCIPAL = 1;
-    private Button btnEnt;
-    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // Toolbar
-        toolbar = findViewById(R.id.toolbar1);
+      //  Toolbar toolbar = findViewById(R.id.toolbar1);
         setSupportActionBar();
         Objects.requireNonNull(getActionBar()).setTitle("Ayer & Hoy");
 

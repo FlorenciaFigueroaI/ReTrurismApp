485504575f6067b48341eafd7e95dc23fe1f0e38
FlorenciaFigueroaI <72564481+FlorenciaFigueroaI@users.noreply.github.com>
2020-10-29 11:19:58 +0100

    Cambios main.
    *Se agregan CuentaFragment, ListadoFotosFragment, MapaFragment

--- app/src/main/java/com/example/dam2pm/MainActivity.java
+++ app/src/main/java/com/example/dam2pm/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.dam2pm;
 
 import android.os.Bundle;
+//import android.view.Menu;
+//import android.view.MenuInflater;
+//import android.view.MenuItem;
+//import android.view.View;
+//import android.widget.LinearLayout;
 import android.widget.Toolbar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentTransaction;
+//import androidx.fragment.app.FragmentTransaction;
+
 
 public class MainActivity extends AppCompatActivity {
     Toolbar toolbar;
-
+  //  LinearLayout linearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,19 +23,59 @@ public class MainActivity extends AppCompatActivity {
         // Toolbar
         toolbar = findViewById(R.id.tlBarBuscador);
         setSupportActionBar();
-
-        // Fragmento Explore
-
-        // Link ayuda: https://developer.android.com/training/basics/fragments/fragment-ui?hl=es
-        FragmentTransaction transaccion = getSupportFragmentManager().beginTransaction(); // creación nueva transacción
-        ExploreFragment frgExplore = new ExploreFragment(); // instancia de transacción
-        transaccion.add(R.id.contMain, frgExplore); // se añade transacción
-        transaccion.commit(); // confirmación del cambio
-
     }
 
     private void setSupportActionBar() {
-
     }
 
+/*
+    // Menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.navigation, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+
+            case R.id.nvCuenta:
+                //  cuenta();
+
+                return true;
+            case R.id.nvgFotos:
+                //  listadoFotos();
+                return true;
+
+            case R.id.nvgMapa:
+                //    mapa();
+                return true;
+
+            case R.id.nvgAjustes:
+                View itemAjustes = findViewById(R.id.nvgAjustes);
+                itemAjustes.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        linearLayout=findViewById(R.id.contMain);
+                        linearLayout.setVisibility(View.GONE);
+                        FragmentTransaction transaccion = getSupportFragmentManager().beginTransaction();
+                        AjustesFragment frgAjustes = new AjustesFragment();
+                        transaccion.add(R.id.contAjustes, frgAjustes);
+                        transaccion.commit();
+                    }
+                });
+
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+*/
+
 }
+

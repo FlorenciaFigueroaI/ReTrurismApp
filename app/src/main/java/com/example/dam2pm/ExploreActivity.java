e9ec9383aca1b7138a76d793695255160c3fa0aa
FlorenciaFigueroaI <72564481+FlorenciaFigueroaI@users.noreply.github.com>
2020-10-30 13:24:12 +0100

    Cambios en Main
    *Se cambia ExploreActivity por MainActivity.java
    *Se agrega AjustesActivity
    *Se añade un fragmento al layout ExploreActivity

---
+++ app/src/main/java/com/example/dam2pm/ExploreActivity.java
@@ -0,0 +1,24 @@
+package com.example.dam2pm;
+
+import android.os.Bundle;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class ExploreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_explore);
+
+
+
+
+
+    }
+
+
+}
+

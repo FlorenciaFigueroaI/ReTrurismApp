1737c34707ad7d4aa129ab5c44536e4c1f566468
FlorenciaFigueroaI <72564481+FlorenciaFigueroaI@users.noreply.github.com>
2020-11-14 17:48:37 +0100

    Cambios en main
    * Se agrega OpenStreetMap en fragment_mapa.xml y MapaFragment

--- app/src/main/java/com/example/dam2pm/MapaFragment.java
+++ app/src/main/java/com/example/dam2pm/MapaFragment.java
@@ -7,19 +7,38 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapController;
+import org.osmdroid.views.MapView;
+
 
 public class MapaFragment extends Fragment {
 
+    View view;
+    MapController mpContrl;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mapa, container, false);
+        view = inflater.inflate(R.layout.fragment_mapa, container, false);
+        MapView mapVw = view.findViewById(R.id.mapaVw);
+        GeoPoint castro = new GeoPoint( -3.2167, 43.3833);
+        mapVw.setBuiltInZoomControls(true);
+        mpContrl = (MapController) mapVw.getController();
+        mpContrl.setCenter(castro);
+        mpContrl.setZoom(15);
+        mapVw.setMultiTouchControls(true);
+
+        // carga layout del fragment map
+       // return inflater.inflate(R.layout.fragment_mapa, container, false);
+
+        return view;
     }
 }

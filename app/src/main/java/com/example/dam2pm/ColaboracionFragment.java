12d9c0c6cf0e709e7dffb2c3a5e8e0692b19b6d6
FlorenciaFigueroaI <72564481+FlorenciaFigueroaI@users.noreply.github.com>
2020-11-23 01:54:26 +0100

    Cambios en main
    * Se agrega interacción con otra aplicación del dispositivo (con la galería)

--- app/src/main/java/com/example/dam2pm/ColaboracionFragment.java
+++ app/src/main/java/com/example/dam2pm/ColaboracionFragment.java
@@ -1,15 +1,29 @@
 package com.example.dam2pm;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import static android.app.Activity.RESULT_OK;
 
 
 public class ColaboracionFragment extends Fragment {
 
+ //   private static final int RESULT_OK = ;
+    ImageView imagen;
+    FloatingActionButton fltActBtn;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -20,7 +34,36 @@ public class ColaboracionFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_colaboracion, container, false);
+        View view = inflater.inflate(R.layout.fragment_colaboracion, container, false);
+
+        imagen = view.findViewById(R.id.imgVwEjem);
+        fltActBtn = view.findViewById(R.id.fltActBtn);
+        fltActBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cargarImagen(); // método para cargar la imagen
+            }
+        });
+
+
+        return view;
+
+    }
+
+    @SuppressLint("IntentReset")
+    private void cargarImagen() {
+        @SuppressLint("IntentReset") Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        intent.setType("image/");
+        startActivityForResult(Intent.createChooser(intent, "Selecciona la aplicación"),10);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode== RESULT_OK){
+            Uri path=data.getData();
+            imagen.setImageURI(path);
+
+        }
     }
 }

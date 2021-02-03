46c7109a3062db268ee82435321158c06d738d4f
FlorenciaFigueroaI <72564481+FlorenciaFigueroaI@users.noreply.github.com>
2021-02-03 19:11:52 +0100

    Cambios en main * Se agrega clase Galeria

---
+++ app/src/main/java/com/example/dam2pm/Galeria.java
@@ -0,0 +1,93 @@
+package com.example.dam2pm;
+
+public class Galeria {
+    Integer galeriaId;
+    String titulo;
+    String descripcion;
+    String ciudad;
+    int anyo;
+    String ruta;
+    Usuario usuarioId;
+
+    public Galeria() {
+    }
+
+    public Galeria(Integer galeriaId, String titulo, String descripcion, String ciudad, int anyo, String ruta, Usuario usuarioId) {
+        this.galeriaId = galeriaId;
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.ciudad = ciudad;
+        this.anyo = anyo;
+        this.ruta = ruta;
+        this.usuarioId = usuarioId;
+    }
+
+    public Integer getGaleriaId() {
+        return galeriaId;
+    }
+
+    public void setGaleriaId(Integer galeriaId) {
+        this.galeriaId = galeriaId;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public int getAnyo() {
+        return anyo;
+    }
+
+    public void setAnyo(int anyo) {
+        this.anyo = anyo;
+    }
+
+    public String getRuta() {
+        return ruta;
+    }
+
+    public void setRuta(String ruta) {
+        this.ruta = ruta;
+    }
+
+    public Usuario getUsuarioId() {
+        return usuarioId;
+    }
+
+    public void setUsuarioId(Usuario usuarioId) {
+        this.usuarioId = usuarioId;
+    }
+
+    @Override
+    public String toString() {
+        return "Galeria{" + "galeriaId=" + galeriaId + ", titulo=" + titulo + ", descripcion=" + descripcion + ", ciudad=" + ciudad + ", anyo=" + anyo + ", ruta=" + ruta + ", usuarioId=" + usuarioId + '}';
+    }
+
+
+
+}
+
+
+
+
+
--- app/src/main/java/com/example/dam2pm/Usuario.java
+++ app/src/main/java/com/example/dam2pm/Usuario.java
@@ -17,4 +17,38 @@ public class Usuario {
         this.apellido = apellido;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" + ", email=" + email + ", nombre=" + nombre + ", apellido=" + apellido + '}';
+    }
+
+
+
+
+
+
 }
